1e5cc54994f9bad3c113874cd8626ebb9641b43b
iam-milad <miladnouri073@hotmail.com>
2025-06-26 18:45:14 +0100

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB(); // Connect to MongoDB
+
+const app = express();
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie parser middleware
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`✅ Server running on port ${port}`);
+});
