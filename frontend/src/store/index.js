2586b5ede2a0b6c8bda23d38db7aed22c214cfb9
iam-milad <miladnouri073@hotmail.com>
2025-07-01 23:30:39 +0100

    add dialog reducer to the store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import callReducer from '../store/callSlice';
+import dialogReducer from '../store/dialogSlice';
 
 const store = configureStore({
   reducer: {
     call: callReducer,
+    dialog: dialogReducer
   },
 });
 
